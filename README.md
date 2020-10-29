d8f8291f2b594def6aeee370e01c6a262e73e2c1
Arkojeet Bera <arkojeetphy007@gmail.com>
2020-10-29 14:44:48 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# News Feed
+This is an android app in which you can get hold of all the national News at one place. The News also can be viewed in the app without using any browser!
+
+Screenshots will be added in the next commit.
+
+### Stay Tuned!!
---
+++ app/src/main/java/com/example/newsfeed/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.newsfeed
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+
+class MainActivity : AppCompatActivity(), NewsItemClicked {
+
+    private lateinit var mAdapter: NewsListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rec = findViewById<RecyclerView>(R.id.recyclerView)
+        rec.layoutManager = LinearLayoutManager(this)
+
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+
+        rec.adapter = mAdapter
+
+    }
+
+    private fun fetchData(){
+        val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=953414072c7b4e758e46d32fa49aaec0"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                            newsJsonObject.getString("title"),
+                            newsJsonObject.getString("author"),
+                            newsJsonObject.getString("url"),
+                            newsJsonObject.getString("urlToImage"),
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+            },
+            {
+
+            }
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    override fun onItemClicked(item: News) {
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(item.url));
+    }
+
+}
